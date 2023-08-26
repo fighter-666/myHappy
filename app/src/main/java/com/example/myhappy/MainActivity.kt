edc5efadda3a08f01aaaed7a5041416e53840a9c
wudm <977657115@qq.com>
2023-08-26 13:54:45 +0800

    1

---
+++ app/src/main/java/com/example/myhappy/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myhappy
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myhappy.ui.theme.MyHappyTheme
+    private lateinit var textView: TextView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
+
